cf1e7e0c5608fe91bbb40ad80c7c866e142ac908
Ralph Largo <devlargs@gmail.com>
2022-06-19 14:06:27 +0800

    feat: initial commit

---
+++ components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import { Box, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+import { FC } from 'react';
+
+const Header: FC = () => (
+  <Box bg="yellow.800" px="4">
+    <Box h="80px" justifyContent="space-between" maxW="1280px" m="auto" d="flex" alignItems="center">
+      <Text color="white" fontSize="18px" fontWeight="bold">
+        Logo
+      </Text>
+      <Box d="flex" color="white">
+        <Link href="/politicians" passHref>
+          <Text
+            mr="4"
+            cursor="pointer"
+            _hover={{
+              color: 'yellow.300',
+            }}
+            transition="0.5s ease-in"
+          >
+            Politicians
+          </Text>
+        </Link>
+        <Link href="/" passHref>
+          <Text
+            cursor="pointer"
+            _hover={{
+              color: 'yellow.300',
+            }}
+            transition="0.5s ease-in"
+          >
+            Speeches
+          </Text>
+        </Link>
+      </Box>
+    </Box>
+  </Box>
+);
+
+export default Header;
---
+++ pages/politicians.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const Politicians: FC = () => {
+  return <>Politicians</>;
+};
+
+export default Politicians;
