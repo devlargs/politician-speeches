d762264e5c3f3eedcf7d5448a407eddc1f219d3f
Ralph Largo <devlargs@gmail.com>
2022-06-20 03:46:58 +0800

    feat: limit title on table if its too long

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ import {
   Flex,
   Grid,
   Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
   Spinner,
   Table,
   TableContainer,
@@ -16,6 +19,7 @@ import {
   Th,
   Thead,
   Tr,
+  useToast,
 } from '@chakra-ui/react';
 import AddSpeechForm from '@components/AddSpeechForm';
 import { DELETE_SPEECH } from '@graphql/mutations/speech';
@@ -24,9 +28,10 @@ import { Speech } from '@graphql/types';
 import dayjs from 'dayjs';
 import Link from 'next/link';
 import { FC, useEffect, useState } from 'react';
-import { AiFillDelete, AiFillEye } from 'react-icons/ai';
+import { AiFillDelete, AiFillEye, AiOutlineSearch } from 'react-icons/ai';
 
 const Home: FC = () => {
+  const [searchText, setSearchText] = useState('');
   const [state, setState] = useState<'view' | 'add'>('view');
   const [getSpeeches, { data, loading }] = useLazyQuery<{
     speeches: Speech[];
@@ -35,6 +40,7 @@ const Home: FC = () => {
       setState('view');
     },
   });
+  const toast = useToast();
   const [deleteSpeech] = useMutation(DELETE_SPEECH);
 
   useEffect(() => {
@@ -45,7 +51,24 @@ const Home: FC = () => {
     <Box px="4" mt="8">
       <Box maxW="1280px" m="auto">
         <Flex w="100%" justifyContent="space-between">
-          <Text fontSize="24px">{state === 'add' ? 'Add a Speech' : 'Speeches'} </Text>
+          <Box d="flex" alignItems="center">
+            <Text mr="4" fontSize="24px">
+              {state === 'add' ? 'Add a Speech' : 'Speeches'}{' '}
+            </Text>
+            {state === 'view' && (
+              <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                  <AiOutlineSearch />
+                </InputLeftElement>
+                <Input
+                  type="text"
+                  placeholder="Search by title"
+                  value={searchText}
+                  onChange={(e): void => setSearchText(e.target.value.toLowerCase())}
+                />
+              </InputGroup>
+            )}
+          </Box>
           <Button
             colorScheme={state === 'view' ? 'facebook' : 'red'}
             onClick={(): void => setState((e) => (e === 'add' ? 'view' : 'add'))}
@@ -81,50 +104,59 @@ const Home: FC = () => {
                         </Thead>
                         <Tbody>
                           {data.speeches.map((speech) => {
-                            return (
-                              <Tr key={speech._id}>
-                                <Td>{speech.title}</Td>
-                                <Td>
-                                  <Flex alignItems="center">
-                                    <Image
-                                      src={speech.politicians[0].imageUrl}
-                                      width={10}
-                                      height={10}
-                                      borderRadius="50%"
-                                      mr="4"
-                                      border="2px solid black"
-                                      alt={`${speech.politicians[0].firstName} image`}
-                                    />
-                                    {speech.politicians[0].firstName} {speech.politicians[0].lastName}
-                                  </Flex>
-                                </Td>
-                                <Td>{dayjs(speech.date).format('MMM DD, YYYY')}</Td>
-                                <Td>
-                                  <Link href={`/speeches/${speech._id}`} passHref>
-                                    <Button colorScheme="blue">
-                                      <AiFillEye />
+                            if (speech.title.toLowerCase().includes(searchText)) {
+                              return (
+                                <Tr key={speech._id}>
+                                  <Td>
+                                    {speech.title.length > 49 ? `${speech.title.substring(0, 40)}..` : speech.title}
+                                  </Td>
+                                  <Td>
+                                    <Flex alignItems="center">
+                                      <Image
+                                        src={speech.politicians[0].imageUrl}
+                                        width={10}
+                                        height={10}
+                                        borderRadius="50%"
+                                        mr="4"
+                                        border="2px solid black"
+                                        alt={`${speech.politicians[0].firstName} image`}
+                                      />
+                                      {speech.politicians[0].firstName} {speech.politicians[0].lastName}
+                                    </Flex>
+                                  </Td>
+                                  <Td>{dayjs(speech.date).format('MMM DD, YYYY')}</Td>
+                                  <Td>
+                                    <Link href={`/speeches/${speech._id}`} passHref>
+                                      <Button colorScheme="blue">
+                                        <AiFillEye />
+                                      </Button>
+                                    </Link>
+                                    <Button
+                                      colorScheme="red"
+                                      ml="4"
+                                      onClick={async (): Promise<void> => {
+                                        try {
+                                          await deleteSpeech({
+                                            variables: {
+                                              id: speech._id,
+                                            },
+                                          });
+                                        } finally {
+                                          void getSpeeches();
+                                          toast({
+                                            title: 'Successfully delete speech',
+                                            status: 'success',
+                                            duration: 2000,
+                                          });
+                                        }
+                                      }}
+                                    >
+                                      <AiFillDelete />
                                     </Button>
-                                  </Link>
-                                  <Button
-                                    colorScheme="red"
-                                    ml="4"
-                                    onClick={async (): Promise<void> => {
-                                      try {
-                                        await deleteSpeech({
-                                          variables: {
-                                            id: speech._id,
-                                          },
-                                        });
-                                      } finally {
-                                        void getSpeeches();
-                                      }
-                                    }}
-                                  >
-                                    <AiFillDelete />
-                                  </Button>
-                                </Td>
-                              </Tr>
-                            );
+                                  </Td>
+                                </Tr>
+                              );
+                            }
                           })}
                         </Tbody>
                       </Table>
